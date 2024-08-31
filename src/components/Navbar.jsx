5331e41c145a2ac21561acc40178265469b2c69c
Dipak Das <98222877+neoochii@users.noreply.github.com>
2024-08-31 21:41:35 +0530

    toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import {navLinks} from "../constants/index.js";
 import {logo,menu ,close} from  '../assets'
 
 const Navbar = () => {
-  const [active,setActive]= useState('`')
+  const [active,setActive]= useState('`');
+  const [toggle, setToogle] = useState(false);
   return (
     <nav
     className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -21,12 +22,22 @@ const Navbar = () => {
           {navLinks.map((link) => (
               <li
                 key={link.id}
-                className={`${active === link.title ? "text-white" : "text-secondary"}`}>
+                className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px]
+                font-medium cursor-pointer transition duration-150 ease-in-out transform -translate-x-1/2`}
+              onClick={()=>{setActive(link.title)}}>
                 <a href={`#${link.id}`}>{link.title}</a>
               </li>
           ))}
 
         </ul>
+        <div className="sm:hidden flex flex-1
+        justify-end items-center ">
+          <img src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain cursor-pointer "
+          onClick={()=> setToogle(!toggle)}/>
+
+        </div>
 
       </div>
 
